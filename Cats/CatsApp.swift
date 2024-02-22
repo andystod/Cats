59f1cd9cbd38e5a3ec8b44b215b6532155c161e6
Andrew Stoddart <andystod@users.noreply.github.com>
2024-02-22 17:24:37 -0500

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Andrew Stoddart on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
